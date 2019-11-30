7624d6c393127890b5cb98c8aac1384e264b3c7b
root <root@ip-172-31-24-89.us-west-2.compute.internal>
2019-11-30 08:35:24 +0000

    new

--- handler.js
+++ handler.js
@@ -16,6 +16,8 @@ mysql.config({
   body: 'everything is alright'
 };
 
+new
+
 module.exports.connectionHandler = (event, context, callback) => {
   console.log(event);
 
@@ -57,7 +59,7 @@ module.exports.connectionHandler = (event, context, callback) => {
 };
 
 const addConnection = async (connectionId) => {
-  let sql = "INSERT INTO Scrum_connectiontable(connectionid) VALUES("+connectionId+");" 
+  let sql = "INSERT INTO Scrum_connectiontable (connectionid) VALUES(?),[connectionId]" 
 
   let results = await mysql.query(sql)
   // let results = await mysql.query('INSERT INTO Scrum_connectiontable(connectionid) VALUES(connectionId)')
